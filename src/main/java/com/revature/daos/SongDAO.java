f3d07890c49750ec0f6d93795b289e74953c14f4
Sidney Bowe <sidneyb1202@gmail.com>
2024-04-18 14:55:10 -0700

    touched up models and DAOs

--- src/main/java/com/revature/daos/SongDAO.java
+++ src/main/java/com/revature/daos/SongDAO.java
@@ -1,8 +1,9 @@
 package com.revature.daos;
 
+import com.revature.models.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SongDAO extends JpaRepository{
+public interface SongDAO extends JpaRepository<Song, Integer>{
 }
--- src/main/java/com/revature/models/Song.java
+++ src/main/java/com/revature/models/Song.java
@@ -1,14 +1,16 @@
 package com.revature.models;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.stereotype.Component;
 
 import java.lang.String;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name="songs")
 @Component
-class Song{
+public class Song{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int songId;
@@ -21,15 +23,24 @@ class Song{
 
 	private String songAlbum;
 
+	@CreationTimestamp
+	private LocalDateTime lastPlayed;
+
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+	@JoinColumn(name = "userId")
+	private User user;
+
 	//Constructors
 	public Song(){
 	}
 
-	public Song(int songID, String songName, String songArtist, String songAlbum){
+	public Song(int songID, String songName, String songArtist, String songAlbum, LocalDateTime lastPlayed, User user){
 		this.songId = songID;
 		this.songName = songName;
 		this.songArtist = songArtist;
 		this.songAlbum = songAlbum;
+		this.lastPlayed = lastPlayed;
+		this.user = user;
 	}
 
 	//Getters and Setters
@@ -41,6 +52,10 @@ class Song{
 
 	public String getSongAlbum() { return songAlbum; }
 
+	public LocalDateTime getLastPlayed() { return lastPlayed; }
+
+	public User getUser() { return user; }
+
 	public void setSongID(int songID) { this.songId = songID; }
 
 	public void setSongName(String songName) { this.songName = songName; }
@@ -48,6 +63,10 @@ class Song{
 	public void setSongArtist(String songArtist) { this.songArtist = songArtist; }
 
 	public void setSongAlbum(String songAlbum) { this.songAlbum = songAlbum; }
+
+	public void setLastPlayed(LocalDateTime lastPlayed) { this.lastPlayed = lastPlayed; }
+
+	public void setUser(User user) { this.user = user; }
 
 	//Custom toString
 	public String toString() { return songArtist + " - " + songName; }
